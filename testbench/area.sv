d03a1203afb4d5df7b44d013c68524d6d785a203
Chaity Sarkar <chaitysarkar2103@gmail.com>
2026-08-31 17:54:14 +0600

    Add my file - modified:   testbench/area.sv - new file:   testbench/array.sv

--- testbench/area.sv
+++ testbench/area.sv
@@ -1,27 +1,26 @@
 module area;
-  int a, b, t_area, s_area;
+ 
+  int a, b, t_area;
+  int s_area[0:9];
 
-  function int tri_area(input int a, b);
+  function int tri_area(int a, b);
     return (0.5 * a * b);
-
   endfunction
 
-  function int squ_area(input int a);
+  function int sq_area(int a);
     return (a ** 2);
-
   endfunction
 
-initial begin
+  initial begin
     a = 5;
     b = 2;
     t_area = tri_area(a,b);
-    $display("Area of triangle (a = %0d, b = %0d):%d", a,b,t_area);
-
-    a = 4;
-    s_area = squ_area(a);
-    $display("Area of square (a =%d):%d", a,s_area);
-
-end
-
+    $display("Area of triangle (a = %0d, b = %0d): %0d", a, b, t_area);
+    
+    for (int i = 0; i < 10; i++) begin
+      s_area[i] = sq_area(i);
+      $display("Area of square (a = %0d,): %0d", i, s_area[i]);
+    end
+  end
 
 endmodule
---
+++ testbench/array.sv
@@ -0,0 +1,30 @@
+module array;
+  int a[10];
+
+  initial begin
+    
+    for (int i = 0; i < 10; i++) begin
+      a[i] = i * i;
+    end  
+
+    for (int i = 0; i < 10; i++) begin
+      $display("a[%0d] : %0d", i, a[i]);
+    end  
+
+    $finish;
+
+  end
+    
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
